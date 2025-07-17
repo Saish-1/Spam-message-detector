f854190d9ad43834b8ee91ca8aff8061ad50a29a
K Saish Dilip Varma <saishdilip03@gmail.com>
2025-07-17 16:10:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@
 import streamlit as st
 import joblib
 import nltk
-nltk.download('punkt')
+nltk==3.8.1
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import string 
